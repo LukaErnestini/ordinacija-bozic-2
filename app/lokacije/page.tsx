7b14446bfdd909aa49d8198f6dc90b141d147669
Luka Ernestini <luka.ernestini@actual-it.si>
2025-01-01 00:43:26 +0100

    Refactor Business Locations Page: Remove unnecessary min-height from container and streamline layout for improved design consistency.

--- app/lokacije/page.tsx
+++ app/lokacije/page.tsx
@@ -5,8 +5,8 @@ export default async function BusinessLocations() {
   const locations = await client.queries.locationConnection();
 
   return (
-    <div className="min-h-screen bg-gray-50 p-8">
-      <h1 className="text-4xl font-bold text-center mb-8 text-gray-900">Nase Lokacije</h1>
+    <div className="p-8">
+      <h1 className="text-4xl font-bold text-center mb-8">Nase Lokacije</h1>
       <div className="flex flex-col md:flex-row justify-center items-center md:items-start space-y-8 md:space-y-0 md:space-x-8">
         <LokacijeClientPage {...locations}></LokacijeClientPage>
       </div>
