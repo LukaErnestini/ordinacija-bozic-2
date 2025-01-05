82dad47b0ddf75c5a1b37bd097b0f34f8a8f3bd0
Luka Ernestini <luka.ernestini@actual-it.si>
2025-01-05 15:14:36 +0100

    refactor home into client-page

---
+++ app/client-page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import {
+  GlobalQuery,
+  ServiceCategoryConnectionQuery,
+  ServiceCategoryConnectionQueryVariables
+} from "@/tina/__generated__/types";
+import { TinaConnectionClientPageProps, TinaQueryClientPageProps } from "@/tina/utils";
+import CategoriesSmall from "@/components/services/categories-small";
+import { tinaField, useTina } from "tinacms/dist/react";
+
+export default function ClientHomePage({
+  categoriesConnectionQuery: categoriesConnectionQuery,
+  globalQuery
+}: {
+  categoriesConnectionQuery: TinaConnectionClientPageProps<
+    ServiceCategoryConnectionQuery,
+    ServiceCategoryConnectionQueryVariables
+  >;
+  globalQuery: TinaQueryClientPageProps<GlobalQuery>;
+}) {
+  const { global } = useTina(globalQuery).data;
+  return (
+    <div className="-mt-nav mx-auto flex flex-col gap-12">
+      <Image
+        data-tina-field={tinaField(global, "landingImage")}
+        className="mx-auto"
+        src={global.landingImage}
+        alt=""
+        width={2048}
+        height={1365}
+      />
+      <h2
+        data-tina-field={tinaField(global, "ourServicesText")}
+        className="text-center text-4xl font-semibold capitalize mb-8"
+      >
+        {global.ourServicesText}
+      </h2>
+      <CategoriesSmall {...categoriesConnectionQuery}></CategoriesSmall>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,15 @@
 import client from "@/tina/__generated__/client";
-import CategoriesSmall from "../components/services/categories-small";
-import Image from "next/image";
+import ClientHomePage from "./client-page";
 
 export default async function Home() {
   // const services = await client.queries.servicesConnection();
   const categories = await client.queries.serviceCategoryConnection();
-  const imageSrc = (await client.queries.global({ relativePath: "global.json" })).data.global.landingImage;
+  const globalQuery = await client.queries.global({ relativePath: "global.json" });
 
   return (
-    <div className="-mt-nav mx-auto flex flex-col gap-12">
-      {imageSrc && (
-        <Image
-          className="mx-auto"
-          src={imageSrc}
-          alt=""
-          width={2048}
-          height={1365}
-        />
-      )}
-      <h2 className="text-center text-4xl font-semibold capitalize mb-8">Naše storitve</h2>
-      <CategoriesSmall {...categories}></CategoriesSmall>
-    </div>
+    <ClientHomePage
+      categoriesConnectionQuery={categories}
+      globalQuery={globalQuery}
+    ></ClientHomePage>
   );
 }
