caf6d14439929266b1419c9ff90e54844b4682b5
Luka Ernestini <luka.ernestini@actual-it.si>
2025-03-23 12:16:56 +0100

    Refactor CategoriesSmall component layout and styling for improved responsiveness and readability

--- components/services/categories-small.tsx
+++ components/services/categories-small.tsx
@@ -13,15 +13,15 @@ export default function CategoriesSmall(
 
   return (
     <section className="pb-12">
-      <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="container mx-auto px-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 lg:gap-6">
           {data?.serviceCategoryConnection.edges?.map((categoryData) => {
             const category = categoryData!.node!;
             return (
               <Link
                 href={category._sys.filename}
                 key={category?.id}
-                className="btn h-56 justify-start flex-nowrap group p-6 bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 flex flex-col items-center text-center"
+                className="btn h-56 justify-start flex-nowrap group xl:p-6 p-4 bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 flex flex-col gap-2 items-center text-center"
               >
                 <Image
                   data-tina-field={tinaField(categoryData?.node, "icon")}
@@ -29,17 +29,16 @@ export default function CategoriesSmall(
                   height={54}
                   src={category.icon}
                   alt={category.title}
-                  className="mb-2"
                 />
                 <h3
                   data-tina-field={tinaField(categoryData?.node, "title")}
-                  className="text-xl font-semibold mb-3"
+                  className="text-lg"
                 >
                   {category.title}
                 </h3>
                 <p
                   data-tina-field={tinaField(categoryData?.node, "shortDescription")}
-                  className="text-gray-600 text-sm line-clamp-3"
+                  className="text-gray-600 text-sm font-normal line-clamp-4"
                 >
                   {category.shortDescription}
                 </p>
